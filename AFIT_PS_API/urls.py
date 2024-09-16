535daa640576fa16f38735c58e4a5fb6f4096c13
John Okojere <59104769+john-okojere@users.noreply.github.com>
2024-09-16 12:35:56 +0100

    Update urls.py

--- AFIT_PS_API/urls.py
+++ AFIT_PS_API/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('payment.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 from django.utils.translation import gettext_lazy as _
 
 admin.site.site_header = "AeroPay Admin"  # Customize the site header
